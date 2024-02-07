1300c601886dc8ad19a2df0454cfca4e4053cff5
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-02-07 19:08:04 +0530

    Time: 15 ms (81.16%) | Memory: 28.5 MB (5.10%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) 
     {
+        int n=nums.size();
         vector<int>pre;
-        vector<int>post(nums.size());
-        vector<int>ans(nums.size());
+        vector<int>post(n);
+        vector<int>ans(n);
         pre.push_back(nums[0]);
-        post[nums.size()-1]=nums[nums.size()-1];
-        for(int i=1;i<nums.size();i++)
+        post[n-1]=nums[n-1];
+        for(int i=1;i<n;i++)
         {
             pre.push_back(pre[i-1] * nums[i]);
-            post[nums.size()-i-1]=post[nums.size()-i]*nums[nums.size()-i-1];
+            post[n-i-1]=post[n-i]*nums[n-i-1];
         }   
-        for(auto x:pre)
-        {
-            cout<<x<<" ";
-        }
-        cout<<endl;
-        for(auto x:post)
-        {
-            cout<<x<<" ";
-        }
-        for(int i=1;i<nums.size()-1;i++)
+        for(int i=1;i<n-1;i++)
         {
             ans[i]=pre[i-1]*post[i+1];
 
         }
        ans[0]=post[1];
-       ans[nums.size()-1]=pre[nums.size()-2];
+       ans[n-1]=pre[n-2];
         return ans;
     }
 
