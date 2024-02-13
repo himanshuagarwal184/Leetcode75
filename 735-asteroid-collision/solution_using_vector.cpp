4b76884bfa205e6d405e0682e73eee1c2c76c0d4
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-02-13 18:30:41 +0530

    Create solution_using_vector.cpp

---
+++ 735-asteroid-collision/solution_using_vector.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) 
+    {
+        vector<int>vt;
+        for(int &a:asteroids)
+        {
+            while(!vt.empty() && a<0 && vt.back()>0)
+            {
+                int sum=a+vt.back();
+                if(sum<0)
+                {
+                    vt.pop_back();
+                }
+                else if(sum > 0)
+                {
+                    a=0;
+                }
+                else
+                {
+                    vt.pop_back();
+                    a=0;
+                }
+            }
+            if(a!=0)
+            {
+                vt.push_back(a);
+            }
+        }
+        return vt;
+    }
+};
