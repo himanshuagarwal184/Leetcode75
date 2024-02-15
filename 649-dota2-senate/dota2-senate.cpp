052daaee0d0ca9c2b7b928829f2dcd0340b8623a
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-02-15 17:42:07 +0530

    Time: 4 ms (84.32%) | Memory: 10.4 MB (21.37%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) 
+    {
+        queue<int>rad,dire;
+        int n=senate.length();
+        for(int i=0;i<n;i++)
+        {
+            if(senate[i]=='R')
+            {
+                rad.push(i);
+            }
+            else
+            {
+                dire.push(i);
+            }
+        }
+        if(rad.size()==0)
+        {
+            return "Dire";
+        }
+        if(dire.size()==0)
+        {
+            return "Radiant";
+        }
+        int i=0;
+        while(!rad.empty() && !dire.empty())
+        {
+            if(rad.front() < dire.front())
+            {
+                dire.pop();
+                int r= rad.front();
+                rad.push(r+n);
+                rad.pop();
+            }else
+            {
+                rad.pop();
+                int d = dire.front();
+                dire.push(d+n);
+                dire.pop();
+            }
+        }
+        if(!rad.empty())
+        {
+            return "Radiant";
+        }
+        return "Dire";
+        
+    }
+};
