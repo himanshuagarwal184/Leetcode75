3db4470920feafd500aae93ee3d6c49f8e2ed4fd
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-02-22 18:48:28 +0530

    Time: 472 ms (26.54%) | Memory: 126.6 MB (40.27%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int count=0;
+    void dfs(int u,int par,unordered_map<int,vector<pair<int,int>>> &graph)
+    {
+        for(auto &p:graph[u])
+        {
+            int v=p.first;
+            int check= p.second;
+            if(v!=par)
+            {
+                if(check==1)
+                {
+                    count++;
+                }
+                dfs(v,u,graph);
+            }
+        }
+    }
+    int minReorder(int n, vector<vector<int>>& connections) 
+    {
+        unordered_map<int,vector<pair<int,int>>> graph;
+
+        for(auto &x: connections)
+        {
+            int a=x[0];
+            int b=x[1];
+            graph[a].push_back({b,1});
+            graph[b].push_back({a,0});
+        }
+
+        dfs(0,-1,graph);
+        return count;
+    }
+};
