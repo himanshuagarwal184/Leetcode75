41fc69409d207f4e3c986119ecc463678deda5dc
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-04-24 19:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (26.53%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int score(vector<int>& nums,int i, int j,int turn)
+    bool predictTheWinner(vector<int>& nums)
     {
-        if(i>j){
-            return 0;
-        }
-        if(turn==0){
-            int s1=nums[i]+score(nums,i+1,j,1);
-            int s2=nums[j] + score(nums,i,j-1,1);
-            return max(s1,s2);
-        }
-        else
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++)
         {
-            int s1=score(nums,i+1,j,0);
-            int s2=score(nums,i,j-1,0);
-            return min(s1,s2);   
+            dp[i][i]=nums[i];
+        }   
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                dp[i][j] = max(nums[i]-dp[i+1][j], nums[j] - dp[i][j-1]);
+            }
         }
-    }
-    bool predictTheWinner(vector<int>& nums) 
-    {
-        int p1=0,p2=0;
-        int i=0,j=nums.size()-1;
-        if(score(nums,i,j,0) >= score(nums,i,j,1)){
-            return true;
-        }
-        return false;
+        return dp[0][n-1] >= 0; 
     }
 };
