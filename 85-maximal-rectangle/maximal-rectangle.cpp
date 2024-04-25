a1bfcbc87c9a42f2c15d636c64de34ffd82f2ce2
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-04-25 15:56:37 +0530

    Time: 77 ms (5.29%) | Memory: 25.8 MB (5.48%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,116 @@
+class Solution {
+public:
+    vector<int>nextSmallerElement(vector<int>heights,int n)
+    {
+        stack<int>s;
+        s.push(-1);
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--)
+        {
+            int curr=heights[i];
+            while(s.top()!=-1 && heights[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    vector<int>prevSmallerElement(vector<int>heights,int n)
+    {
+        stack<int>s;
+        s.push(-1);
+        vector<int>ans(n);
+        for(int i=0;i<n;i++)
+        {
+            int curr=heights[i];
+            while(s.top()!=-1 && heights[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+
+        vector<int> next(n);
+        next = nextSmallerElement(heights,n);
+
+        vector<int> prev(n);
+        prev = prevSmallerElement(heights,n);
+        int area = INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            int l=heights[i];
+            if(next[i] == -1){
+                next[i]=n;
+            }
+            int b=next[i]-prev[i]-1;
+            
+            area =  max(l*b,area);
+        }
+        return area;
+    }
+
+    void calculateHistogram(vector<vector<char>> mat,vector<vector<int>> &dp)
+    {
+        // vector<vector<int>>ret(dp.size(),vector<int>(dp[0].size(),0));
+        
+        for(int i=0;i<dp[0].size();i++)
+        {
+            if(mat[0][i]=='1')
+            {
+                dp[0][i]=1;
+            }
+            else{
+                dp[0][i]=0;
+            }
+        }
+
+        for(int i=1;i<dp.size();i++)
+        {
+            for(int j=0;j<dp[0].size();j++)
+            {
+                if(mat[i][j]=='1')
+                {
+                    dp[i][j]=dp[i-1][j]+1;
+                }
+                else
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+    }
+
+    
+
+    int maximalRectangle(vector<vector<char>>& matrix) 
+    {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        // cout<<m<<n;
+        vector<vector<int>>dp (m,vector<int>(n,0));
+        calculateHistogram(matrix,dp);
+        for(int i=0;i<dp.size();i++)
+        {
+            for(int j=0;j<dp[i].size();j++)
+            {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        int maxx=0;
+    for(int i=0;i<dp.size();i++)
+    {
+        vector<int>s = dp[i];
+        maxx = max(largestRectangleArea(s),maxx);
+    }
+    return maxx;      
+    }
+};
