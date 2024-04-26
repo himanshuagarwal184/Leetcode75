69d0861187cfc5a51d4c70f03e0785260eb7ed6d
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-04-26 19:30:18 +0530

    Time: 27 ms (28.55%) | Memory: 75.9 MB (80.75%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -29,9 +29,9 @@ public:
             ListNode *q = new ListNode(x);
             curr->next=q;
             curr=curr->next;
-            if(l1){
+            if(l1!=NULL){
             l1=l1->next;}
-            if(l2){
+            if(l2!=NULL){
             l2=l2->next;}
         }    
         if(carry!=0){
