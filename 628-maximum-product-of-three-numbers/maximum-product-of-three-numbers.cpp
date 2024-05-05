11b3bbfbf89cdc669e17296423f6c6b84edfacd0
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-05-06 00:09:46 +0530

    Time: 37 ms (39.50%) | Memory: 31.4 MB (5.74%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -5,11 +5,6 @@ public:
         int n=nums.size()-1;
         int pro1 = nums[0]*nums[1]*nums[n];
         int pro2 = nums[n]*nums[n-1]*nums[n-2];
-        
-        if(pro1 >  pro2)
-        {
-            return pro1;
-        }
-        return pro2;
+        return max(pro1,pro2);
     }
 };
