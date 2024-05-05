800994ccf60dbb68dc7e48f5069a1af85f020f4a
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-05-06 00:20:23 +0530

    Time: 37 ms (92.54%) | Memory: 17.4 MB (50.30%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,28 +3,20 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size(),n=matrix[0].size();
         int i=0,j=n-1;
-        int num = matrix[i][j];
-        if(num==target){
-            return true;
-        }
         
         while(i<m && j>=0)
         {
-            num = matrix[i][j];
-            cout<<num<<" ";
-            if(num == target){
+            if(matrix[i][j] == target){
                 return true;
             }
-            else if(num < target){
+            else if(matrix[i][j] < target){
                 i++;
             }
-            else if(num > target){
+            else {
                 j--;
             }
             
         }
-
-        cout<<num;
         return false;
     }
 };
