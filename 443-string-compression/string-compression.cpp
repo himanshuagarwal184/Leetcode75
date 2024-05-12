0778b4f4fcb1c4b1d3561e68afcab90577265f64
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-05-12 23:37:34 +0530

    Time: 2 ms (81.83%) | Memory: 12.7 MB (26.99%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    vector<char> arr(string num)
-    {
-        int n=num.length();
-        vector<char> a(n);
-        for(int i=0;i<n;i++)
-        {
-            a[i]=num[i];
-        }
-        return a;
-    }
     int compress(vector<char>& chars) 
     {
 
         int index=0;
-        int i=0;
-        while(i<chars.size())
+        int i=0,j=0;
+        while(j<chars.size())
         {
-            int j=i;
             while(j<chars.size() && chars[j]==chars[i])
             {
                 j++;
@@ -26,10 +15,9 @@ public:
             if(j-i > 1)
             {
                 string count = to_string(j-i);
-                vector<char> q = arr(count);
-                for(int i=0;i<q.size();i++)
+                for (char c:count)
                 {
-                    chars[index++]=q[i];
+                    chars[index++]=c;
                 }
 
             }
