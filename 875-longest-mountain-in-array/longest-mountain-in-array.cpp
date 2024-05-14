d144a577ba381899b5fd2dbb871b41e872bef094
himanshuagarwal184 <117289844+himanshuagarwal184@users.noreply.github.com>
2024-05-14 23:15:27 +0530

    Time: 15 ms (48.65%) | Memory: 20.8 MB (68.77%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -14,7 +14,8 @@ public:
             }
         }
         int maxx=0;
-        for(int i=1;i<n-1;)
+        int i=1;
+        while(i<n-1)
         {
             if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1]))
             {
